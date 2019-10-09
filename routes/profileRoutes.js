4a4fbc17051edaa0be2e64459cd64fd95dcdd926
Michael Nos <mnos9897@gmail.com>
2019-10-09 19:27:15 -0400

    fixed router error with google auth

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const checkLogin = require("../config/middleware/index.js");
 
-router.get("/", checkLogin, (req, res) => {
+router.get("/", checkLogin.isLoggedIn, (req, res) => {
   res.render("profile", {
     user : req.user
   });
