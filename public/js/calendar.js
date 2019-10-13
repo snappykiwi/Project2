a6ca7444fc8a0ff7adbfe3ae97f6d683b7388279
kpoole133 <kpoole133@gmail.com>
2019-10-13 13:28:36 -0400

    fix calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,37 +1,34 @@
-$(document).ready(function() {
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
 
-  // CALENDAR LOGIC
-  let now = new Date();
-  let currentMonth = now.getMonth();
-  let currentYear = now.getFullYear();
-  let selectYear = document.getElementById("year");
-  let selectMonth = document.getElementById("month");
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-  let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-  let monthAndYear = document.getElementById("monthAndYear");
-  // showCalendar(currentMonth, currentYear);
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
 
 
-  function next() {
+function next() {
     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
     currentMonth = (currentMonth + 1) % 12;
     showCalendar(currentMonth, currentYear);
-  }
+}
 
-  function previous() {
+function previous() {
     currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
     currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
     showCalendar(currentMonth, currentYear);
-  }
+}
 
-  function jump() {
+function jump() {
     currentYear = parseInt(selectYear.value);
     currentMonth = parseInt(selectMonth.value);
     showCalendar(currentMonth, currentYear);
-  }
+}
 
-  function showCalendar(month, year) {
+function showCalendar(month, year) {
 
     let firstDay = (new Date(year, month)).getDay();
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
@@ -49,38 +46,35 @@ $(document).ready(function() {
     // creating all cells
     let date = 1;
     for (let i = 0; i < 6; i++) {
-      // creates a table row
-      let row = document.createElement("tr");
+        // creates a table row
+        let row = document.createElement("tr");
 
-      //creating individual cells, filing them up with data.
-      for (let j = 0; j < 7; j++) {
-        if (i === 0 && j < firstDay) {
-          let cell = document.createElement("td");
-          let cellText = document.createTextNode("");
-          cell.appendChild(cellText);
-          row.appendChild(cell);
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+
+            else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+
         }
-        else if (date > daysInMonth) {
-          break;
-        }
 
-        else {
-          let cell = document.createElement("td");
-          let cellText = document.createTextNode(date);
-          if (date === now.getDate() && year === now.getFullYear() && month === now.getMonth()) {
-            cell.classList.add("bg-info");
-          } // color today's date
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-          date++;
-        }
-
-
-      }
-
-      tbl.appendChild(row); // appending each row into calendar body.
+        tbl.appendChild(row); // appending each row into calendar body.
     }
-
-  };
-
-});
+}
