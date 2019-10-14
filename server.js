2afbbbde69e3defd8d10c6fce7e7da29dac0f03f
Michael Nos <mnos9897@gmail.com>
2019-10-14 09:31:51 -0400

    email works with oauth2

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const express     = require("express"),
       authRoutes  = require("./routes/authRoutes.js"),
       profRoutes  = require("./routes/profileRoutes.js"),
       nodemailer  = require("nodemailer"),
-      hbs         = require("nodemailer-express-handlebars");
+      hbs         = require("nodemailer-express-handlebars"),
+      { google }  = require("googleapis"),
+      OAuth2      = google.auth.OAuth2;
 
 
 //requiring routes
@@ -30,109 +32,41 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// let transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASS
-//   }
-// });
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
 
-// transporter.use('compile', hbs({
-//   viewEngine: 'express-handlebars',
-//   viewPath: '/views/'
-// }));
+oauth2Client.setCredentials({
+  refresh_token: process.env.refresh_token
+});
+const accessToken = oauth2Client.getAccessToken();
 
-// let options = {
-//   from: 'schedulingapp742@gmail.com',
-//   to: 'kpoole133@gmail.com',
-//   subject: 'Test',
-//   text: 'does this work?',
-//   template: 'email',
-//   context: {
-//       name: 'testtesttest'
-//   }
-// };
+const smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: "schedulingapp742@gmail.com",
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.refresh_token,
+    accessToken: accessToken
+  }
+});
 
-// transporter.sendMail(options, (err, data) => {
-//   if (err) {
-//       return console.log('Error ' + err);
-//   }
-//   return console.log('Email sent!');
-// });
+const mailOptions = {
+  from: "schedulingapp742@gmail.com",
+  to: "schedulingapp742@gmail.com",
+  subject: "This is a test Email with OAuth",
+  generateTextFromHTML: true,
+  html: "<b>test</b>"
+};
 
-// ********************
-
-// const options = {
-//   viewEngine: {
-//     extName: '.hbs',
-//     partialsDir: './views',
-//     layoutsDir: './views',
-//     defaultLayout: 'email.handlebars',
-//   },
-//   viewPath: './views',
-//   extName: '.handlebars',
-// };
-
-// transporter.use('compile', hbs(options));
-// let mail = {
-//    from: 'schedulingapp742@gmail.com',
-//    to: 'kpoole133@gmail.com',
-//    subject: 'Test',
-//    template: 'email',
-//    context: {
-//        name: 'Name'
-//    },
-//    text: 'Did this really send?'
-// }
-// transporter.sendMail(mail, function(err, info) {
-//   if (err) {
-//     console.log("error " + err);
-//   } else {
-//     console.log("Message sent");
-//   }
-// });
-
-// 'use strict';
-// // const nodemailer = require('nodemailer');
-
-// // async..await is not allowed in global scope, must use a wrapper
-// async function main() {
-//     // Generate test SMTP service account from ethereal.email
-//     // Only needed if you don't have a real mail account for testing
-//     let testAccount = await nodemailer.createTestAccount();
-
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//         host: 'smtp.gmail.com',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         // service: "gmail",
-//         auth: {
-//             user: 'schedulingapp742@gmail.com', // generated ethereal user
-//             pass: 'scheduling' // generated ethereal password
-//         }
-//     });
-
-//     // send mail with defined transport object
-//     let info = await transporter.sendMail({
-//         from: '"Test Email" <schedulingapp742@gmail.com>', // sender address
-//         to: 'schedulingapp742@gmail.com', // list of receivers
-//         subject: 'Hello ✔', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: '<b>Hello world?</b>' // html body
-//     });
-
-//     console.log('Message sent: %s', info.messageId);
-//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-//     // Preview only available when sending through an Ethereal account
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-// }
-
-// main().catch(console.error);
-
+smtpTransport.sendMail(mailOptions, (error, response) => {
+  error ? console.log(error) : console.log(response);
+  smtpTransport.close();
+})
 
 // Handlebars
 app.engine(
@@ -147,48 +81,6 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// app.use("./auth", authRoutes);
-// app.use("./profile", profRoutes);
-
-// 'use strict';
-// const nodemailer = require('nodemailer');
-
-// // async..await is not allowed in global scope, must use a wrapper
-// async function main() {
-//     // Generate test SMTP service account from ethereal.email
-//     // Only needed if you don't have a real mail account for testing
-//     let testAccount = await nodemailer.createTestAccount();
-
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//         host: 'smtp.ethereal.email',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: testAccount.user, // generated ethereal user
-//             pass: testAccount.pass // generated ethereal password
-//         }
-//     });
-
-//     // send mail with defined transport object
-//     let info = await transporter.sendMail({
-//         from: '"Fred Foo 👻" <schedulingapp742@gmail.com>', // sender address
-//         to: 'schedulingapp742@gmail.com', // list of receivers
-//         subject: 'Hello ✔', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: '<b>Hello world?</b>' // html body
-//     });
-
-//     console.log('Message sent: %s', info.messageId);
-//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-//     // Preview only available when sending through an Ethereal account
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-// }
-
-// main().catch(console.error);
-
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
@@ -196,8 +88,6 @@ var syncOptions = { force: false };
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
-
-
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
